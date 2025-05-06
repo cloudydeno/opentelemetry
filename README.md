e073e1cb2033b16ed7b2de2fd725daae6ab9c900
Daniel Lamando <dan@danopia.net>
2025-05-07 00:24:29 +0200

    Bring in more-detailed async instrumentation impl

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![CI](https://github.com/cloudydeno/opentelemetry/actions/workflows/deno-ci.yml/badge.svg?branch=main)
 
-# `@cloudydeno/opentelemetry`
+# `@cloudydeno/opentelemetry` on JSR
 
 A Deno-oriented build of [opentelemetry-js](https://github.com/open-telemetry/opentelemetry-js),
 with extra Deno-specific integrations.
 
 * [Published on JSR](https://jsr.io/@cloudydeno/opentelemetry) since `v0.10.0`.
-* Also [published to `/x/`](https://deno.land/x/observability) (with one JSR dependency).
+* Previous versions were [published to `/x/`](https://deno.land/x/observability).
 
 ## example usage
 
@@ -14,6 +14,7 @@ with extra Deno-specific integrations.
 // set up the SDK:
 import 'jsr:@cloudydeno/opentelemetry/register';
 // Can also use `deno --unstable-otel` instead of the /register import.
+// See more setup in the deno documentation on OTel (short for opentelemetry)
 
 // Wrap an async function in a span:
 import { traceAsyncFunc } from 'jsr:@cloudydeno/opentelemetry/instrumentation/async.ts';
--- instrumentation/async.ts
+++ instrumentation/async.ts
@@ -1,33 +1,157 @@
 import {
   type Span,
+  type SpanOptions,
+  type Tracer,
   SpanKind,
-  SpanStatusCode,
   context,
   trace,
 } from "../opentelemetry/api.js";
 
-// Richer version of this here:
-// https://github.com/danopia/dist-app-meteor/blob/main/imports/lib/tracing.ts
+export class LogicTracer {
+  tracer: Tracer;
+  requireParent: boolean;
+  constructor(options: {
+    name: string;
+    version?: string;
+    requireParent?: boolean;
+  }) {
+    this.tracer = trace.getTracer(options.name, options.version);
+    this.requireParent = options.requireParent ?? false;
 
-const tracer = trace.getTracer('async_func');
+    this.InternalSpan = this.InternalSpan.bind(this);
+  }
 
-export async function traceAsyncFunc<T>(spanName: string, func: (span: Span) => Promise<T>): Promise<T> {
-  const span = tracer.startSpan(spanName, {
-    kind: SpanKind.INTERNAL,
-  });
-  try {
-    const spanContext = trace.setSpan(context.active(), span);
-    const result = await context.with(spanContext, () => func(span));
-    span.end();
-    return result;
-  } catch (thrown: unknown) {
-    const err = thrown as Error;
-    span.recordException(err);
-    span.setStatus({
-      code: SpanStatusCode.ERROR,
-      message: err.message,
+  InternalSpan<
+    Tthis,
+    Targs extends unknown[],
+    Tret extends unknown,
+  >(
+    originalMethod: (...args: Targs) => Promise<Tret>,
+    context: ClassMethodDecoratorContext<Tthis, (this: Tthis, ...args: Targs) => Promise<Tret>>,
+  ): (...args: Targs) => Promise<Tret> {
+    const asyncSpan = this.asyncSpan.bind(this);
+    return function (this: Tthis, ...args: Targs): Promise<Tret> {
+      return asyncSpan(context.name.toString(), {
+        kind: SpanKind.INTERNAL,
+      }, () => originalMethod.apply(this, args));
+    };
+  }
+
+  /** Runs an async function within a new span, and then ends the span upon completion */
+  async asyncSpan<Tret>(
+    spanName: string,
+    options: SpanOptions,
+    func: (span: Span | null) => Promise<Tret> | Tret,
+  ): Promise<Tret> {
+    // if (!trace.getActiveSpan()) {
+    //   const err = new Error('no parent span for '+spanName);
+    //   if (!err.stack?.includes('new Computation')) console.warn('WARN:', err.stack);
+    // }
+    if (this.requireParent && !trace.getActiveSpan()) {
+      return await func(null);
+    }
+
+    const span = this.tracer.startSpan(spanName, {
+      kind: SpanKind.INTERNAL,
+      ...options,
     });
-    span.end();
-    throw err;
+
+    try {
+      const spanContext = trace.setSpan(context.active(), span);
+      return await context.with(spanContext, () => func(span));
+    } catch (err) {
+      span.recordException(err as Error);
+      throw err;
+    } finally {
+      span.end();
+    }
+  }
+
+  /** Runs an sync function within a new span, and then ends the span upon return */
+  syncSpan<Tret>(
+    spanName: string,
+    options: SpanOptions,
+    func: (span: Span | null) => Tret,
+  ): Tret {
+    // if (!trace.getActiveSpan()
+    //     && !spanName.startsWith('ShellSession task:')
+    //     && !spanName.startsWith('MessageHost rpc:')) {
+    //   const err = new Error('no parent span for '+spanName);
+    //   if (!err.stack?.includes('new Computation')) console.warn('WARN:', err.stack);
+    // }
+    if (this.requireParent && !trace.getActiveSpan()) {
+      return func(null);
+    }
+
+    const span = this.tracer.startSpan(spanName, {
+      kind: SpanKind.INTERNAL,
+      ...options,
+    });
+
+    try {
+      const spanContext = trace.setSpan(context.active(), span);
+      return context.with(spanContext, () => func(span));
+    } catch (err) {
+      span.recordException(err as Error);
+      throw err;
+    } finally {
+      span.end();
+    }
+  }
+
+  /** Wraps an async function and returns a new function which creates a span around the promise */
+  wrapAsyncWithSpan<
+    Targs extends unknown[],
+    Tret,
+  >(
+    spanName: string,
+    options: SpanOptions,
+    func: (...args: Targs) => Promise<Tret>,
+    thisArg?: unknown,
+  ): (...args: Targs) => Promise<Tret> {
+    return (...args: Targs) =>
+      this.asyncSpan(spanName, options, () =>
+        func.apply(thisArg, args));
+  }
+
+  /** Wraps a sync function and returns a new function which creates a span around the promise */
+  wrapSyncWithSpan<
+    Targs extends unknown[],
+    Tret,
+  >(
+    spanName: string,
+    options: SpanOptions,
+    func: (...args: Targs) => Tret,
+  ): (...args: Targs) => Tret {
+    return (...args: Targs) =>
+      this.syncSpan(spanName, options, () =>
+        func.apply(null, args));
+  }
+
+  tracedInterval<T>(func: () => Promise<T>, delayMs: number): number {
+    const funcName = func.name || `${func.toString().slice(0, 50)}...` || '(anonymous)';
+    return setInterval(this.wrapAsyncWithSpan(funcName, {
+      attributes: {
+        'timer.interval_ms': delayMs,
+      }
+    }, func), delayMs);
+  }
+
+  tracedTimeout<T>(func: () => Promise<T>, delayMs: number): number {
+    const funcName = func.name || `${func.toString().slice(0, 50)}...` || '(anonymous)';
+    return setTimeout(() => this.asyncSpan(funcName, {
+      attributes: {
+        'timer.timeout_ms': delayMs,
+      }
+    }, func), delayMs);
   }
 }
+
+const defaultTracer = new LogicTracer({
+  name: 'async_func',
+});
+export const asyncSpan: LogicTracer['asyncSpan'] = defaultTracer.asyncSpan.bind(defaultTracer);
+export const syncSpan: LogicTracer['syncSpan'] = defaultTracer.syncSpan.bind(defaultTracer);
+export const wrapAsyncWithSpan: LogicTracer['wrapAsyncWithSpan'] = defaultTracer.wrapAsyncWithSpan.bind(defaultTracer);
+export const tracedInterval: LogicTracer['tracedInterval'] = defaultTracer.tracedInterval.bind(defaultTracer);
+export const tracedTimeout: LogicTracer['tracedTimeout'] = defaultTracer.tracedTimeout.bind(defaultTracer);
